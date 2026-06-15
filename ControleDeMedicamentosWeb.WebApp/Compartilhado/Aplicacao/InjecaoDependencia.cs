be51b6c9f43ff1454a548b0d013860bca35714a5
vinicius.poli <alemaumvp@hotmail.com>
2026-06-15 14:01:41 -0300

    Corrigido erro de Pacientes e Funcionarios na View

--- ControleDeMedicamentosWeb.WebApp/Compartilhado/Aplicacao/InjecaoDependencia.cs
+++ ControleDeMedicamentosWeb.WebApp/Compartilhado/Aplicacao/InjecaoDependencia.cs
@@ -1,6 +1,8 @@
 using ControleDeMedicamentosWeb.WebApp.Modulos.ModuloFornecedor.Aplicacao;
 using ControleDeMedicamentosWeb.WebApp.Modulos.ModuloMedicamento.Aplicacao;
 using ControleDeMedicamentosWeb.WebApp.Modulos.ModuloEstoque.Aplicacao;
+using ControleDeMedicamentosWeb.WebApp.Modulos.ModuloPaciente.Aplicacao;
+using ControleDeMedicamentosWeb.WebApp.Modulos.ModuloFuncionario.Aplicacao;
 
 namespace ControleDeMedicamentosWeb.WebApp.Compartilhado.Aplicacao;
 
@@ -9,7 +11,9 @@ public static class InjecaoDependencia
     public static void AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<ServicoFornecedor>();
+        services.AddScoped<ServicoPaciente>();
         services.AddScoped<ServicoMedicamento>();
+        services.AddScoped<ServicoFuncionario>();
         services.AddScoped<ServicoEstoque>();
     }
 }
