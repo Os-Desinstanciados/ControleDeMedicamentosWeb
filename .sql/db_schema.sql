441ff4f3ee443d98e8b1c69c97ec34436c28c6a9
vinicius.poli <alemaumvp@hotmail.com>
2026-06-19 01:53:36 -0300

    Adicionadas Seleções de Requisições de Entrada e Saída

--- .sql/db_schema.sql
+++ .sql/db_schema.sql
@@ -1,21 +1,3 @@
-IF DB_ID('ControleDeMedicamentosWeb') IS NULL
-BEGIN
-    CREATE DATABASE [ControleDeMedicamentosWeb];
-END;
-
-
-USE [ControleDeMedicamentosWeb];
-GO
-
-
-CREATE TABLE [dbo].[TBFornecedor] (
-[Id] uniqueidentifier NOT NULL,
-[Nome] nvarchar(100) NOT NULL,
-[Telefone] nvarchar(100) NOT NULL,
-[Cnpj] nvarchar(100) NOT NULL,
-PRIMARY KEY ([Id])
-);
-
 CREATE TABLE [dbo].[TbPaciente] (
 [Id] uniqueidentifier NOT NULL,
 [Nome] nvarchar(100) NOT NULL,
@@ -25,11 +7,11 @@ CREATE TABLE [dbo].[TbPaciente] (
 PRIMARY KEY ([Id])
 );
 
-CREATE TABLE [dbo].[TBFuncionario] (
+CREATE TABLE [dbo].[TBRequisicaoEntrada] (
 [Id] uniqueidentifier NOT NULL,
-[Nome] nvarchar(100) NOT NULL,
-[Telefone] nvarchar(15) NOT NULL,
-[Cpf] nvarchar(14) NOT NULL,
+[FuncionarioId] uniqueidentifier NOT NULL,
+[MedicamentoId] uniqueidentifier NOT NULL,
+[Quantidade] int NOT NULL,
 PRIMARY KEY ([Id])
 );
 
@@ -41,11 +23,109 @@ CREATE TABLE [dbo].[TBMedicamento] (
 PRIMARY KEY ([Id])
 );
 
+CREATE TABLE [dbo].[TBFornecedor] (
+[Id] uniqueidentifier NOT NULL,
+[Nome] nvarchar(100) NOT NULL,
+[Telefone] nvarchar(100) NOT NULL,
+[Cnpj] nvarchar(100) NOT NULL,
+PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [dbo].[TBRequisicao] (
+[Id] uniqueidentifier NOT NULL,
+[DataCriacao] datetime2(7) NOT NULL,
+PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [dbo].[TBFuncionario] (
+[Id] uniqueidentifier NOT NULL,
+[Nome] nvarchar(100) NOT NULL,
+[Telefone] nvarchar(15) NOT NULL,
+[Cpf] nvarchar(14) NOT NULL,
+PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [dbo].[TBMedicamentoPrescrito] (
+[RequisicaoSaidaId] uniqueidentifier NOT NULL,
+[MedicamentoId] uniqueidentifier NOT NULL,
+[Quantidade] int NOT NULL,
+PRIMARY KEY ([RequisicaoSaidaId], [MedicamentoId])
+);
+
+CREATE TABLE [dbo].[TBRequisicaoSaida] (
+[Id] uniqueidentifier NOT NULL,
+[PacienteId] uniqueidentifier NOT NULL,
+PRIMARY KEY ([Id])
+);
+
+
+ALTER TABLE [dbo].[TBRequisicaoEntrada]
+ADD CONSTRAINT [FK_RequisicaoEntrada_TBMedicamento]
+FOREIGN KEY ([MedicamentoId]) 
+REFERENCES [dbo].[TBMedicamento]([Id])
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+
+
+ALTER TABLE [dbo].[TBRequisicaoEntrada]
+ADD CONSTRAINT [FK_TBRequicisaoEntrada_TBFuncionario]
+FOREIGN KEY ([FuncionarioId]) 
+REFERENCES [dbo].[TBFuncionario]([Id])
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+
+
+ALTER TABLE [dbo].[TBRequisicaoEntrada]
+ADD CONSTRAINT [FK_RequisicaoEntrada_TBRequisicao]
+FOREIGN KEY ([Id]) 
+REFERENCES [dbo].[TBRequisicao]([Id])
+ON DELETE CASCADE
+ON UPDATE NO ACTION;
+
+
 
 ALTER TABLE [dbo].[TBMedicamento]
 ADD CONSTRAINT [FK_TBMedicamento_TBFornecedor]
 FOREIGN KEY ([FornecedorId]) 
 REFERENCES [dbo].[TBFornecedor]([Id])
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+
+
+ALTER TABLE [dbo].[TBMedicamentoPrescrito]
+ADD CONSTRAINT [FK_MedicamentoPrescrito_TBMedicamento]
+FOREIGN KEY ([MedicamentoId]) 
+REFERENCES [dbo].[TBMedicamento]([Id])
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+
+
+ALTER TABLE [dbo].[TBMedicamentoPrescrito]
+ADD CONSTRAINT [FK_TBMedicamentoPrescrito_TBRequisicaoSaida]
+FOREIGN KEY ([RequisicaoSaidaId]) 
+REFERENCES [dbo].[TBRequisicaoSaida]([Id])
+ON DELETE CASCADE
+ON UPDATE NO ACTION;
+
+
+
+ALTER TABLE [dbo].[TBRequisicaoSaida]
+ADD CONSTRAINT [FK_RequisicaoSaida_TBRequisicao]
+FOREIGN KEY ([Id]) 
+REFERENCES [dbo].[TBRequisicao]([Id])
+ON DELETE CASCADE
+ON UPDATE NO ACTION;
+
+
+
+ALTER TABLE [dbo].[TBRequisicaoSaida]
+ADD CONSTRAINT [FK_TBRequisicaoSaida_TBPaciente]
+FOREIGN KEY ([PacienteId]) 
+REFERENCES [dbo].[TbPaciente]([Id])
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
